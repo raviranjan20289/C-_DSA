bb2239404626297a7666f284b304d197817ea250
raviranjan202 <mailraviranjan28@gmail.com>
2023-11-06 12:40:38 +0530

    sort 0 and 1

---
+++ Array/sort01.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+
+void printArray(int arr[], int size){
+    for(int i = 0; i< size; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void sortOne(int arr[], int n){
+    int left = 0, right = n-1;
+
+    while(left< right){
+
+        while(arr[left] == 0){
+            left++;
+        }
+
+        while(arr[right] == 1){
+            right --;
+        }
+
+        if(left < right){
+            swap(arr[left], arr[right]);
+            left++;
+            right--;
+        }
+    }
+}
+
+
+int main(){
+
+    int arr[6] = {1,1,0,1,0,0};
+
+    sortOne(arr, 6);
+    printArray(arr, 6);
+}
